6b825bb1582cd94dea41a0283d100310eadb9c97
jozzi <dominik.malantowicz@gmail.com>
2020-04-13 16:02:05 +0200

    [refactoring] Remove redundant comments

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,22 +7,14 @@ require "pry"
 require "capybara/rails"
 require "shoulda/matchers"
 
-# Add additional requires below this line. Rails is not loaded until this point!
-
 Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryBot::Syntax::Methods
   config.include Capybara::DSL
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
   config.use_transactional_fixtures = true
 end
 
