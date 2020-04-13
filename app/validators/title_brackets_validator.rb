33c3cd87ed3fc90ab217a4d88946df28edc9add8
jozzi <dominik.malantowicz@gmail.com>
2020-04-13 23:27:19 +0200

    Implement TitleBracketsValidator

---
+++ app/validators/title_brackets_validator.rb
@@ -0,0 +1,22 @@
+class TitleBracketsValidator < ActiveModel::Validator
+  PAIRS = {
+    "]" => "[",
+    "}" => "{",
+    ")" => "("
+  }.freeze
+
+  def validate(record)
+    queue = []
+    record.title.each_char.with_index do |char, index|
+      next queue << [char, index] if PAIRS.value?(char)
+
+      if PAIRS.key?(char)
+        prev_char, prev_index = queue.pop
+        return record.errors[:title] << "contains unclosed brackets" if prev_char != PAIRS[char]
+        return record.errors[:title] << "has empty brackets" if prev_index + 1 == index
+      end
+    end
+
+    record.errors[:title] << "contains unclosed brackets" unless queue.empty?
+  end
+end
