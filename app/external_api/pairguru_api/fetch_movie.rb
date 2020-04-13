5be1178f675724b61571d5053bf18b504d534cb5
jozzi <dominik.malantowicz@gmail.com>
2020-04-13 20:29:34 +0200

    [refactoring] Create Dry::BaseAction module to encapsulate all Dry methods calls in one place

--- app/external_api/pairguru_api/fetch_movie.rb
+++ app/external_api/pairguru_api/fetch_movie.rb
@@ -1,11 +1,6 @@
-require "dry/monads/do"
-require "dry/monads/result"
-require "dry/matcher/result_matcher"
-
 module PairguruApi
   class FetchMovie
-    include Dry::Monads[:result, :do]
-    include Dry::Matcher.for(:call, with:Dry::Matcher::ResultMatcher)
+    include Dry::BaseAction
 
     MOVIE_URL = "#{DOMAIN}/api/v1/movies/%<movie_title>s".freeze
     POSTER_URL = "#{DOMAIN}%<poster>s".freeze
