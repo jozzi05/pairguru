25e3be1042d5a6eb535f765b0744b41dcce25b22
jozzi <dominik.malantowicz@gmail.com>
2020-04-13 16:07:44 +0200

    [refactoring] Move shoulda_matchers setup to external file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,6 @@ require "spec_helper"
 require File.expand_path("../config/environment", __dir__)
 require "rspec/rails"
 require "pry"
-require "shoulda/matchers"
 
 Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 
@@ -13,13 +12,5 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryBot::Syntax::Methods
-
   config.use_transactional_fixtures = true
-end
-
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
 end
