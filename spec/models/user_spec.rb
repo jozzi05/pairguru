86d0c54d93f56363c5e5dc334e892c9850c04de3
jozzi <dominik.malantowicz@gmail.com>
2020-04-13 15:03:07 +0200

    Revert "Add failing test to test CI pipeline"
    This reverts commit 7e00f6a3fa617d18f6b6f6b7578b76e343a1cd0e.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,5 @@ describe User do
   it { is_expected.to allow_value("48 999-888-777").for(:phone_number) }
   it { is_expected.not_to allow_value("+48 aaa bbb ccc").for(:phone_number) }
   it { is_expected.not_to allow_value("aaa +48 aaa bbb ccc").for(:phone_number) }
-  it { is_expected.to allow_value("+48 999 888 777\naseasd").for(:phone_number) }
+  it { is_expected.not_to allow_value("+48 999 888 777\naseasd").for(:phone_number) }
 end
