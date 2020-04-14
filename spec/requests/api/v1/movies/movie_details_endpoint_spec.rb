972ad4804261b3637daf85bed956ef9b83a154cc
jozzi <dominik.malantowicz@gmail.com>
2020-04-14 22:45:25 +0200

    Add regression test to check if flow works also for ids on which contract is gonna fail

--- spec/requests/api/v1/movies/movie_details_endpoint_spec.rb
+++ spec/requests/api/v1/movies/movie_details_endpoint_spec.rb
@@ -4,18 +4,36 @@ RSpec.describe "GET /api/v1/movies/:id - movie details endpoint", type: :request
   subject(:make_request) { get "/api/v1/movies/#{movie_id}" }
 
   context "with movie id missing in DB" do
-    let(:movie_id) { 91919191919191 }
+    context "with integer id" do
+      let(:movie_id) { 91919191919191 }
 
-    before { make_request }
+      before { make_request }
 
-    it "returns status code 404" do
-      expect(response).to have_http_status(404)
+      it "returns status code 404" do
+        expect(response).to have_http_status(404)
+      end
+
+      it "returns corresponding errors" do
+        expect(parsed_errors).to contain_exactly(
+          an_object_matching(code: "resource_not_found", key: "id")
+        )
+      end
     end
 
-    it "returns corresponding errors" do
-      expect(parsed_errors).to contain_exactly(
-        an_object_matching(code: "resource_not_found", key: "id")
-      )
+    context "with string id" do
+      let(:movie_id) { "super_random_id" }
+
+      before { make_request }
+
+      it "returns status code 404" do
+        expect(response).to have_http_status(404)
+      end
+
+      it "returns corresponding errors" do
+        expect(parsed_errors).to contain_exactly(
+          an_object_matching(code: "resource_not_found", key: "id")
+        )
+      end
     end
   end
 
